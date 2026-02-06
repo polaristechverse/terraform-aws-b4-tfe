7ad07379c77897468ed06a33b18a7a184bb4c1e4
kalyan <kalyan@gmail.com>
2026-02-06 07:28:23 +0530

    added new subnet 4

--- network.tf
+++ network.tf
@@ -5,7 +5,7 @@ module "Network" {
   vpc_cidr           = "10.20.0.0/16"
   env                = "Prod"
   PublicSubnet_Cidr  = ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24"]
-  PrivateSubnet_Cidr = ["10.20.10.0/24", "10.20.20.0/24", "10.20.30.0/24"]
+  PrivateSubnet_Cidr = ["10.20.10.0/24", "10.20.20.0/24", "10.20.30.0/24", "10.20.40.0/24"]
   az                 = ["us-east-1a", "us-east-1b", "us-east-1c"]
   NGW                = module.Nat.ngw_id
 }
